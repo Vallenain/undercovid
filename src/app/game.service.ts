86a2ff669738c32232878fd130fb859e2a05e6b6
Vallenain <vallenain@free.fr>
2020-03-21 23:54:53 +0100

    create game if there is none open, retrieve other player and game env

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection, DocumentReference } from '@angular/fire/firestore';
 import { map, take } from "rxjs/operators";
-import { from } from "rxjs"
+import { from, BehaviorSubject } from "rxjs"
 
-import { User, UserService } from './user.service';
+import { User, UserService, Player, PLAYER_ROLE } from './user.service';
 
 export enum GAME_STATUS {
   OPEN = "OPEN",
@@ -24,22 +24,66 @@ export interface Game {
 export class GameService {
 
   private user: User;
+  private _player: Player;
+  private _game: Game;
+  private _gameRef: DocumentReference;
+  game: BehaviorSubject<Game>;
+  player: BehaviorSubject<Player>;
+  players: BehaviorSubject<Player[]>;
 
   constructor(private afs: AngularFirestore, private userService: UserService) {
     userService.user.subscribe(user => {
       this.user = user;
+    });
+    this.game = new BehaviorSubject(undefined);
+    this.player = new BehaviorSubject(undefined);
+    this.players = new BehaviorSubject(undefined);
+  }
+
+  attachGameWatchers() {
+    this.afs.doc<Game>("games/"+this._game.id).valueChanges().subscribe(game => {
+      this._game.status = game.status;
+      this.game.next(this._game);
+    });
+    this.afs.collection<Player>("games/"+this._game.id+"/players").valueChanges().subscribe(players => {
+      this.players.next(players);
     })
   }
 
-  joinGame(gameRef) {
-    return this.afs.collection<User>(`games/${gameRef.id}/players`).doc(this.user.id).set({
+  joinGame(gameSnapshot, isMaster=false) {
+    var playerToCreate = {
+      id: this.user.id,
       name: this.user.name,
-      createdAt: this.user.createdAt
+      createdAt: this.user.createdAt,
+      isMaster: isMaster,
+      nbPoints: 0,
+      eliminated: false,
+      joinedAt: new Date(),
+      role: PLAYER_ROLE.UNASSIGNED
+    }
+    return this.afs.collection<Player>(`games/${gameSnapshot.id}/players`).doc(this.user.id).set(playerToCreate).then(() => {
+      this._game = gameSnapshot.data();
+      this._game.id = gameSnapshot.id;
+      this._gameRef = gameSnapshot.ref;
+      this._player = playerToCreate;
+      this.game.next(this._game);
+      this.player.next(this._player);
+      this.attachGameWatchers();
     });
   }
 
   createAndJoinGame() {
-
+    var gameToCreate = {
+      startedAt: new Date(),
+      status: GAME_STATUS.OPEN
+    };
+    return this.afs.collection<Game>('games').add(gameToCreate).then(gameRef => {
+      return this.afs.doc<Game>('games/'+gameRef.id).snapshotChanges().pipe(take(1)).toPromise().then(action => {
+        if(action)
+          return this.joinGame(action.payload, true);
+        return Promise.reject("Could not find newly created game " + gameRef.id);
+      })
+    })
   }
 
   joinOrCreateGame() {
@@ -47,9 +91,9 @@ export class GameService {
       return Promise.reject("No user found");
 
     return this.afs.collection<Game>('games', ref => ref.where("status", "==", GAME_STATUS.OPEN))
-    .snapshotChanges().pipe(take(1)).toPromise().then(gameRefs => {
-        if(gameRefs.length > 0) {
-          return this.joinGame(gameRefs[0].payload.doc);
+    .snapshotChanges().pipe(take(1)).toPromise().then(actions => {
+        if(actions.length > 0) {
+          return this.joinGame(actions[0].payload.doc);
         }
         return this.createAndJoinGame();
     })
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,14 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import {Game, GAME_STATUS, GameService} from '../game.service';
+import {Player, PLAYER_ROLE} from '../user.service';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
 
-  constructor( private gameService: GameService) {}
+  game: Game;
+  player: Player;
+  players: Player[];
+
+  constructor( private gameService: GameService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.gameService.player.subscribe(player => {
+      if(!player) {
+        this.router.navigate(['/welcome']);
+        return;
+      }
+      this.player = player
+    });
+    this.gameService.game.subscribe(game => {
+      if(!game) {
+        this.router.navigate(['/welcome']);
+        return;
+      }
+      this.game = game
+    });
+    this.gameService.players.subscribe(players => {
+      this.players = players;
+    })
+  }
 
 }
