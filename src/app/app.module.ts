5e0dd7fdbe4b2382f1150835364d336f82a449d0
Vallenain <vallenain@free.fr>
2020-03-21 12:50:55 +0100

    fixed Angular Material issue with Angular Ivy (npm ci did the trick)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,19 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { UiComponentsModule } from './ui-components/ui-components.module'
+import { GameComponent } from './game/game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    UiComponentsModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
---
+++ src/app/ui-components/ui-components.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatSliderModule } from '@angular/material/slider';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatSliderModule
+  ],
+  exports: [
+    MatSliderModule
+  ]
+})
+export class UiComponentsModule { }
